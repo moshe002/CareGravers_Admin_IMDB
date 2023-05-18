a12afbfc1ee400984647cd89f731e870a8dac47e
Moses <mosesfat@gmail.com>
2023-05-18 16:10:02 +0800

    sidebar

---
+++ src/javascript/sidebar.js
@@ -0,0 +1,168 @@
+/* 
+    - on click a tag bg must be white
+    - text must be black
+    - icon must be black
+    - other a tags must be on default (bg blue, white text)
+*/
+
+const dashboard = document.getElementById('dashboard');
+const users = document.getElementById('users');
+const deceased_info = document.getElementById('deceased_info');
+const explorer = document.getElementById('explorer');
+const inquiries = document.getElementById('inquiries');
+const reservations = document.getElementById('reservations');
+const bookings = document.getElementById('bookings');
+const pricing = document.getElementById('pricing');
+const payments = document.getElementById('payments');
+
+const sidebar = [
+    dashboard, 
+    users, 
+    deceased_info, 
+    explorer, 
+    inquiries, 
+    reservations,
+    bookings,
+    pricing,
+    payments
+];
+
+window.addEventListener('load', function(){
+    dashboard.click();
+    dashboard.style.boxShadow = "0px 10px 10px rgba(0, 0, 0, 0.1)";
+    const dashboard_img = document.getElementById('dashboard_img');
+    dashboard_img.src = "../assets/icons/dark_dashboard_icon.png";
+});
+
+
+sidebar.forEach(element => {
+    element.addEventListener('click', () => {
+        switch(element.id) {
+            case 'dashboard':
+                const dashboard_img = document.getElementById('dashboard_img');
+                if(element === document.activeElement){
+                    element.style.boxShadow = "0px 10px 10px rgba(0, 0, 0, 0.1)";
+                    console.log('active', element.id)
+                    dashboard_img.src = "../assets/icons/dark_dashboard_icon.png";
+                    element.addEventListener('focusout', () => {
+                        dashboard_img.src = "../assets/icons/white_dashboard_icon.png";
+                        element.style.boxShadow = "0px 0px 0px";
+                    });
+                }
+                break;
+            case 'users':
+                const users_img = document.getElementById('users_img');
+                if(element === document.activeElement){
+                    element.style.boxShadow = "0px 10px 10px rgba(0, 0, 0, 0.1)";
+                    console.log('active', element.id)
+                    users_img.src = "../assets/icons/dark_users_icon.png";
+                    element.addEventListener('focusout', () => {
+                        users_img.src = "../assets/icons/white_users_icon.png";
+                        element.style.boxShadow = "0px 0px 0px";
+                    });
+                }
+                break;
+            case 'deceased_info':
+                const deceased_info_img = document.getElementById('deceased_info_img');
+                if(element === document.activeElement){
+                    element.style.boxShadow = "0px 10px 10px rgba(0, 0, 0, 0.1)";
+                    console.log('active', element.id)
+                    deceased_info_img.src = "../assets/icons/dark_deceased_icon.png";
+                    element.addEventListener('focusout', () => {
+                        deceased_info_img.src = "../assets/icons/white_deceased_icon.png";
+                        element.style.boxShadow = "0px 0px 0px";
+                    });
+                }
+                break;
+            case 'explorer':
+                const explorer_img = document.getElementById('explorer_img');
+                if(element === document.activeElement){
+                    element.style.boxShadow = "0px 10px 10px rgba(0, 0, 0, 0.1)";
+                    console.log('active', element.id)
+                    explorer_img.src = "../assets/icons/dark_explorer_icon.png";
+                    element.addEventListener('focusout', () => {
+                        explorer_img.src = "../assets/icons/white_explorer_icon.png";
+                        element.style.boxShadow = "0px 0px 0px";
+                    });
+                }
+                break;
+            case 'inquiries':
+                const inquiries_img = document.getElementById('inquiries_img');
+                if(element === document.activeElement){
+                    element.style.boxShadow = "0px 10px 10px rgba(0, 0, 0, 0.1)";
+                    console.log('active', element.id)
+                    inquiries_img.src = "../assets/icons/dark_inquiries_icon.png";
+                    element.addEventListener('focusout', () => {
+                        inquiries_img.src = "../assets/icons/white_inquiries_icon.png";
+                        element.style.boxShadow = "0px 0px 0px";
+                    });
+                }
+                break;
+            case 'reservations':
+                const reservations_img = document.getElementById('reservations_img');
+                if(element === document.activeElement){
+                    element.style.boxShadow = "0px 10px 10px rgba(0, 0, 0, 0.1)";
+                    console.log('active', element.id)
+                    reservations_img.src = "../assets/icons/dark_reservations_icon.png";
+                    element.addEventListener('focusout', () => {
+                        reservations_img.src = "../assets/icons/white_reservations_icon.png";
+                        element.style.boxShadow = "0px 0px 0px";
+                    });
+                }
+                break;
+            case 'bookings':
+                const bookings_img = document.getElementById('bookings_img');
+                if(element === document.activeElement){
+                    element.style.boxShadow = "0px 10px 10px rgba(0, 0, 0, 0.1)";
+                    console.log('active', element.id)
+                    bookings_img.src = "../assets/icons/dark_bookings_icon.png";
+                    element.addEventListener('focusout', () => {
+                        bookings_img.src = "../assets/icons/white_bookings_icon.png";
+                        element.style.boxShadow = "0px 0px 0px";
+                    });
+                }
+                break;
+            case 'pricing':
+                const pricing_img = document.getElementById('pricing_img');
+                if(element === document.activeElement){
+                    element.style.boxShadow = "0px 10px 10px rgba(0, 0, 0, 0.1)";
+                    console.log('active', element.id)
+                    pricing_img.src = "../assets/icons/dark_pricing_icon.png";
+                    element.addEventListener('focusout', () => {
+                        pricing_img.src = "../assets/icons/white_pricing_icon.png";
+                        element.style.boxShadow = "0px 0px 0px";
+                    });
+                }
+                break;
+            case 'payments':
+                const payments_img = document.getElementById('payments_img');
+                if(element === document.activeElement){
+                    element.style.boxShadow = "0px 10px 10px rgba(0, 0, 0, 0.1)";
+                    console.log('active', element.id)
+                    payments_img.src = "../assets/icons/dark_payments_icon.png";
+                    element.addEventListener('focusout', () => {
+                        payments_img.src = "../assets/icons/white_payments_icon.png";
+                        element.style.boxShadow = "0px 0px 0px";
+                    });
+                }
+                break;
+        };
+    });
+});
+
+/*
+    - on click button content div must be changed to 
+    corresponding php file 
+
+*/
+
+// function loadContent(file) {
+//     var xhttp = new XMLHttpRequest();
+//     xhttp.onreadystatechange = function() {
+//       if (this.readyState === 4 && this.status === 200) {
+//         document.getElementById("content_div").innerHTML = this.responseText;
+//       }
+//     };
+//     xhttp.open("GET", file, true);
+//     xhttp.send();
+//   }
