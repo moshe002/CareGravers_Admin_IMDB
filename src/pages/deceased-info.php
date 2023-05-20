70779fe813c5b49913e6a03a620e9a747d265ea2
Moses <mosesfat@gmail.com>
2023-05-20 15:22:07 +0800

    content, sidebar, topnavbar changes

--- src/pages/deceased-info.php
+++ src/pages/deceased-info.php
@@ -16,24 +16,23 @@
 	<meta name="keywords" content="">
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v6.0.0-beta3/css/all.css" integrity="...your-integrity-value-here..." crossorigin="anonymous" />
     <link href="https://unpkg.com/tailwindcss@2.2.19/dist/tailwind.min.css" rel=" stylesheet">
-	<!--Replace with your tailwind.css once created-->
+	<!-- Replace with your tailwind.css once created -->
 
 
-	<!--Regular Datatables CSS-->
+	<!-- Regular Datatables CSS -->
 	<link href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css" rel="stylesheet">
-	<!--Responsive Extension Datatables CSS-->
+	<!-- Responsive Extension Datatables CSS -->
 	<link href="https://cdn.datatables.net/responsive/2.2.3/css/responsive.dataTables.min.css" rel="stylesheet">
     <link rel="stylesheet" href="../css/deceased-info.css">
 
 </head>
 <body class="bg-gray-100 text-gray-900 tracking-wider leading-normal">
-    <!--sidebar-->
-    <?php include 'C:\xampp 8\htdocs\CareGravers_Admin_IMDB\src\components\navbar.php';?>   
-    <!--Container-->
+    <!-- sidebar -->
+    <!-- Container -->
     <div class="container p-16" id="users"> 
     <h3><strong>DECEASED INFORMATION</strong></h3><br><br>
-        <!--Card-->
-        <div id='recipients' class="p-8 mt-6 lg:mt-0 rounded shadow shadow-2xl bg-white">
+        <!-- Card -->
+        <div id='recipients' class="p-8 mt-6 lg:mt-0 rounded shadow-2xl bg-white">
             <table id="example" class="stripe hover" style="width:100%; padding-top: 1em;  padding-bottom: 1em;">
             <thead class="border-b border-primary-200 bg-primary-100 text-neutral-800">
                 <th>ID</th>  
@@ -79,12 +78,12 @@
                 ?>  
             </table>
         </div>
-        <!--/Card-->
+        <!-- /Card -->
     </div>
-    <!--/container-->
+    <!-- /container -->
     <!-- jQuery -->
     <script type="text/javascript" src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
-    <!--Datatables -->
+    <!-- Datatables -->
     <script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
     <script src="https://cdn.datatables.net/responsive/2.2.3/js/dataTables.responsive.min.js"></script>
     <script>
--- src/pages/explorer.php
+++ src/pages/explorer.php
@@ -1,12 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Explorer</title>
-</head>
-<body>
-    <h1>explorer</h1>
-</body>
-</html>
+<?php 
+
+?>
+<h1>explorer</h1>
