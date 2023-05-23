3cca9d182f3868adfdb512472a786c81af8cbba7
KimKellyKim <paraderok9@gmail.com>
2023-05-24 05:48:32 +0800

    kaluy an unta among grado

--- src/pages/approve.php
+++ src/pages/approve.php
@@ -8,6 +8,7 @@
 
     $g_ID= $_GET['gravesiteID'];
     $userID=$_GET['userID'];
+    $availability=$_GET['availability'];
     
     $conn = new mysqli($servername, $username, $password, $dbname);
     if ($conn->connect_error) {
@@ -16,12 +17,12 @@
     
     $sql = "INSERT INTO r_gravestatus (gravesiteID, userID) VALUES ($g_ID, $userID)";
     $sql2 = "UPDATE requestreservation SET approval='1' WHERE requestreservation.userID = $userID";
-    if ($conn->multi_query($sql . ';' . $sql2) === TRUE) {
-        echo "Approval updated successfully";
-        header("Location: reservation.php");
-        exit();
-    } else {
-        echo "Error updating approval: " . $conn->error;
-    }
+        if ($conn->multi_query($sql . ';' . $sql2) === TRUE) {
+            echo "Approval updated successfully";
+            header("Location: reservation.php");
+            exit();
+        } else {
+            echo "Error updating approval: " . $conn->error;
+        }
     
 ?>
